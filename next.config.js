723c57e388eea334447209fb045fa3e269ef01e7
Anatoly <chesnkokov.a@industry-soft.ru>
2022-03-27 18:05:52 +0300

    add firebase

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
-const withPlugins = require('next-compose-plugins');
-const withFonts = require('next-fonts');
+const withPlugins = require("next-compose-plugins");
+const withFonts = require("next-fonts");
+
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  distDir: "build",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    loader: "imgix",
+    path: "https://anatoly-chesnokov.web.app/",
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/contact": { page: "/contact" },
+      "/featured": { page: "/featured" },
+      "/hero": { page: "/hero" },
+      "/jobs": { page: "/jobs" },
+      "/projects": { page: "/projects" },
+    };
+  },
+};
 
-module.exports = withPlugins([[withFonts]], nextConfig)
+module.exports = withPlugins([[withFonts]], nextConfig);
